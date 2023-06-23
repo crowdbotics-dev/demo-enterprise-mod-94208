5c0c570141fb9e1fba28a8e1a861381020513dc7
Jan Acuna <jan.acuna@crowdbotics.com>
2023-06-23 10:43:31 -0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "demo-enterprise-mod-94208",
   url: "https://demo-enterprise-mod-94208.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
